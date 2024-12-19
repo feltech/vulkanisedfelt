1a159c5c2b2e154919aaaff40641ff902280a662
David Feltell <dave@feltell.net>
2024-12-19 00:15:55 +0000

    Add vk_enum_string_helper to macros header
    Attempt to satisfy misc-include-cleaner in files where VK_CHECK
    is used.

--- src/macros.hpp
+++ src/macros.hpp
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2024 David Feltell
 #pragma once
+#include <vulkan/vk_enum_string_helper.h> // NOLINT(*-include-cleaner)
+
 #define VK_CHECK(func, msg)                                                                \
 	do { /* NOLINT(cppcoreguidelines-avoid-do-while)  */                                   \
 		if (const VkResult result = func; result != VK_SUCCESS)                            \
